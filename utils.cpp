33a779bcf125844e6290f81b221f68ac249ab3d6
skd <zxzteh@gmail.com>
2025-03-11 00:15:00 +0300

    Delete files

--- utils.cpp
+++
@@ -1,8 +0,0 @@
-#include "utils.h"
-
-std::vector<uint8_t> qByteArrayToVector(const QByteArray& byteArray)
-{
-    const uint8_t* data = reinterpret_cast<const uint8_t*>(byteArray.data());
-    size_t size = byteArray.size();
-    return std::vector<uint8_t>(data, data + size);
-}
--- utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-#include <qcontainerfwd.h>
-#include <qstringview.h>
-
-
-
-#endif // UTILS_H
